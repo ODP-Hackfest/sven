3f3e843678a2d343e6cc7899e61e4aa8e8e19bb8
C.S. Lim <chaelim@gmail.com>
2014-12-09 17:41:10 -0800

    Updated static files and new default root file.

--- app/app.py
+++ app/app.py
@@ -2,8 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def root():
+    return app.send_static_file('./App/Home/Home.html')
 
 if __name__ == "__main__":
     app.run()
+
--- app/static/Scripts/_references.js
+++ app/static/Scripts/_references.js
@@ -1,8 +1,8 @@
-﻿/* Required to correctly initalize Office.js for intellisense */
+﻿/// <reference path="jquery-1.9.1.js" />
+/* Required to correctly initalize Office.js for intellisense */
 /// <reference path="_officeintellisense.js" />
 /* Use offline copy of Office.js for intellisense */
-/// <reference path="office/1.0/office.js" />
+// /// <reference path="office/1.0/office.js" />
 /* Use online copy of Office.js for intellisense */
-// /// <reference path="https://appsforoffice.microsoft.com/lib/1.0/hosted/outlook-15.debug.js" />
-// /// <reference path="https://appsforoffice.microsoft.com/lib/1.0/hosted/office.js" />
-/// <reference path="jquery-1.10.2.js" />
+/// <reference path="https://appsforoffice.microsoft.com/lib/1.0/hosted/outlook-15.debug.js" />
+/// <reference path="https://appsforoffice.microsoft.com/lib/1.0/hosted/office.js" />
